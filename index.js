f7af34f7f5110c8dde089588fcb3e21bf3964c79
Jesus Lazaro <jesuslazaro87@gmail.com>
2024-12-09 13:45:50 +0000

    finish

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+    const {
+      originalname,
+      mimetype,
+      size,
+    } = req.file;
+    res.json({
+     name: originalname,
+     type: mimetype,
+     size,
+    });
+});
 
 
 const port = process.env.PORT || 3000;
